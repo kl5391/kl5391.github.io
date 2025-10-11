a686ac23063fdb29e6f7c33fc6ba2d7272e92ad5
Kyle Long <=>
2025-10-11 14:23:45 -0400

    fixing resume implementation

--- src/resume-viewer.jsx
+++ src/resume-viewer.jsx
@@ -1,18 +1,12 @@
-import { Document, Page } from "react-pdf";
+const resume = "/kyle_long_resume.pdf";
 
 export default function ResumeViewer() {
   return (
     <div>
-      <iframe
-        src="src\assets\kyle_long_resume.pdf"
-        width="100%"
-        height="800px"
-        title="My Resume"
-      >
+      <iframe src={resume} width="100%" height="800px" title="My Resume">
         <p>
           Your browser does not support iframes. You can
-          <a href="src\assets\kyle_long_resume.pdf">download the PDF</a>{" "}
-          instead.
+          <a href={resume}>download the PDF</a> instead.
         </p>
       </iframe>
     </div>
