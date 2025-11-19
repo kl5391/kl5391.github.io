c90719a31dd4c4b0445aa1260b76a608a10d66a9
Kyle Long <=>
2025-11-19 09:19:12 -0500

    the final solution

--- src/resume-viewer.jsx
+++ src/resume-viewer.jsx
@@ -2,13 +2,11 @@ const resume = "/kyle_long_resume.pdf";
 
 export default function ResumeViewer() {
   return (
-    <div>
-      <iframe src={resume} title="My Resume">
-        <p>
-          Your browser does not support iframes. You can
-          <a href={resume}>download the PDF</a> instead.
-        </p>
-      </iframe>
-    </div>
+    <iframe src={resume} title="My Resume">
+      <p>
+        Your browser does not support iframes. You can
+        <a href={resume}>download the PDF</a> instead.
+      </p>
+    </iframe>
   );
 }
